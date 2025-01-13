4b79266a77ece56b284e3d955045bd8e5b32c1ee
wladimirbix <wladimir.yakovlev@bix-consulting.de>
2025-01-13 13:42:21 +0100

    edited main, ruff.toml and git ignore

--- src/main.py
+++ src/main.py
@@ -1,3 +1,33 @@
-import ruff
+import math
 
-print("Hello World!")
+# Variablen
+pi_value = math.pi
+radius = 5
+circle_area = pi_value * radius**2
+
+# Liste
+fruits = ["apple", "banana", "cherry"]
+
+
+# Dictionary
+person = {"name": "John", "age": 30, "city": "New York"}
+
+
+# Funktion
+def greet(name):
+    return f"Hello, {name}!"
+
+
+# Funktion aufrufen
+greeting = greet(person["name"])
+
+# Ausgabe
+
+
+print(f"The area of the circle is: {circle_area}")
+
+
+print(f"Fruits list: {fruits}")
+print(f"Person details: {person}")
+
+print(greeting)
